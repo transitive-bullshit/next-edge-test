f640f3443c11016591a6ba14677542a7d969f4a2
Travis Fischer <fisch0920@gmail.com>
2022-10-18 12:45:52 -0400

    feat: test ky with edge function

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,17 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import { NextRequest, NextResponse } from 'next/server'
+import ky from 'ky'
 
-type Data = {
-  name: string
+export default async function handler(req: NextRequest) {
+  const data = await ky
+    .post('https://httpbin.org/post', {
+      json: { test: true }
+    })
+    .json()
+  console.log(data)
+
+  return NextResponse.json(data)
 }
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' })
+export const config = {
+  runtime: 'experimental-edge'
 }
